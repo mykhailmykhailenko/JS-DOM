c2a961ee7ed51ad5802f678779d0046879deef14
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-17 19:09:57 +0300

    DOM welcome

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-/*
-Написати функцію createAdder, повертає іншу функцію. Повернена функція має прийняти число в якості аргументу і скласти його з аргументом createAdder
-createAdder(n)
-*/
-
-
-// function createAdder(n) {
-//     return function(m){   
-//         return n = n+m
-//     }
+// function click() {
+//     alert('Achievement unlocked')
 // }
 
-const createAdder = (n) => (m) => n+=m; 
 
-createAdder(5)(10) //виклик функції, що повертається з попередньої функції
+const btnCollection = document.querySelectorAll('button');
+
+for (const btn of btnCollection) {
+    btn.innerText = 'DON`T CLICK';
+}
