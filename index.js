192ccd98dd3b4d122de6893de76855cfe86fc2cb
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-18 22:51:03 +0300

    Coodinates task resolve

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const form = document.querySelector('form');
+/*
+Зробити дів розмірами 200 на 200, кольором фона і бордером
+Поряд з ним зробити невеликий елемент з бордером, де відображати координати натиснення всередині діва.
+*/
 
-form.addEventListener('submit', (event)=>{
-    event.preventDefault();
-    console.log(event)
+const div = document.querySelector('.field');
+
+div.addEventListener('click', (event) => {
+    console.log(event.clientX, event.clientY);
+    const section = document.querySelector('#coodinats');
+    section.innerText = `X: ${event.clientX} \n Y: ${event.clientY}`;
 })
