d4251bcc5f4c64940bdecc7427f0068ca396322c
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-18 22:47:56 +0300

    stopPropagation()

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-document.addEventListener('keydown', handler);
-document.addEventListener('click', handler);
+document.addEventListener('click', clickHandler,{capture: true});
+document.body.addEventListener('click', clickHandler, true);
 
+const parent = document.querySelector('#parent');
+parent.addEventListener('click', clickHandler, true);
 
+const btn = document.querySelector('button');
+btn.addEventListener('click', clickHandler, true);
 
-function handler(event) {
-    console.log(event);
+function clickHandler(event) {
+    console.log(event.currentTarget);
+    if(event.currentTarget === document.querySelector('#parent')) {
+        event.stopPropagation(); //Зупиняє занурення події
+        console.log('Button is bisy');
+    }
 }
 
 
-
-const ev = new PointerEvent('click');
-document.dispatchEvent(ev);
+/*
+Шлях події
+1. Фаза занурення (Capture phase)
+Подія стається на рівні ОС, передається браузеру, браузер "занурює" об'єкт по ланцюжку до того об'єкта, на якому подія спрацювала.
+2. Фаза цілі (Target phase)
+Об'єкт події дійшов до елемента, на якому подія сталась.
+3. Фаза сплиття (Bubbling phase)
+Об'єкт події "спливає" наверх до рівня браузера, попутно активуючі всі обробники події, які зустріне.
+*/
