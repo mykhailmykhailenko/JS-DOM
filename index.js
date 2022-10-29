f3363afbb76ac14be01b83ed9ead65f01287ecf9
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-29 09:45:19 +0300

    userSlider task

--- index.js
+++ index.js
@@ -31,3 +31,38 @@ nextBtn.addEventListener('click', buttonHandler('next'));
 
 
 updateView();
+
+
+
+const userDataBase = [
+    {
+        firstName: 'John',
+        lastName: ' Snow',
+        avatar: './images/136acde59b8ec0fc271f534e3f5a6494.jpeg',
+        email: 'test@mail.com'
+    },
+    {
+        firstName: 'Alex',
+        lastName: ' Doe',
+        avatar: './images/1617079715_51-p-oboi-priroda-vecher-53.jpg',
+        email: 'test@mail.com'
+    },
+    {
+        firstName: 'Gram',
+        lastName: ' How',
+        avatar: './images/1622211351_27-oir_mobi-p-russkaya-priroda-priroda-krasivo-foto-28.jpg',
+        email: 'test@mail.com'
+    },
+    {
+        firstName: 'Nike',
+        lastName: ' Bow',
+        avatar: './images/1623706543_34-oir_mobi-p-krasivaya-priroda-mira-priroda-krasivo-fot-35.jpg',
+        email: 'test@mail.com'
+    },
+    {
+        firstName: 'Pond',
+        lastName: ' Gow',
+        avatar: './images/Живая-природа.jpg',
+        email: 'test@mail.com'
+    }
+]
