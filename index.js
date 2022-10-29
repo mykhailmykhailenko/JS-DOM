a41c4276c1106bd5bcbe810036bbd6b70a583ed2
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-29 14:19:22 +0300

    Converter v1

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const btnCollection = document.querySelectorAll('button');
-const img = document.querySelector('img')
 
-for (const btn of btnCollection) {
-    btn.addEventListener('click', clickHandler)
+const form = document.querySelector('form');
+const resultField = document.querySelector('#resultField');
+const KOEFFICIENT = 36.6;
+
+form.addEventListener('submit', submitHandler);
+
+function submitHandler (event) {
+    event.preventDefault();
+    const {target: {amount: {value}}} = event;
+    if(Number.isNaN(Number(value))) {
+        resultField.textContent = 'Amount must be a number';
+    } else {
+        resultField.textContent = value * KOEFFICIENT;
+    }
 }
 
-function clickHandler (event) {
-   img.setAttribute ('src', event.target.dataset.src)
-}
+
+
+/*
+Маємо input та article.
+Зробити конвертер валют.
+Користувач вводить в input число в долларах, при відправці форми в article вивести конвертовану суму в грн.
+*/
