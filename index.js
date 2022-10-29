6dad2948ee7e52d13db36faae46ac44f5c808c74
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-29 12:23:18 +0300

    MouseOver+Leave switch buttons

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const h1 = document.querySelector('h1');
-console.log(h1);
-h1.textContent = 'THE TEXT';
+const btn1 = document.querySelector('.btn1')
+const btn2 = document.querySelector('.btn2')
 
+btn1.addEventListener('mouseover', changeText)
+btn2.addEventListener('mouseover', changeText)
+
+btn1.addEventListener('mouseleave', changeText)
+btn2.addEventListener('mouseleave', changeText)
+
+function changeText () {
+        const tmp = btn1.innerText;
+        btn1.innerText = btn2.innerText;
+        btn2.innerText = tmp
+}
 
