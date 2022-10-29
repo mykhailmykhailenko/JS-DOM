b3a842ee53c753512458beca9b69869668718684
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-29 12:48:16 +0300

    data-*

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const btn1 = document.querySelector('.btn1')
-const btn2 = document.querySelector('.btn2')
+const btnCollection = document.querySelectorAll('section > button');
 
-btn1.addEventListener('mouseover', changeText)
-btn2.addEventListener('mouseover', changeText)
 
-btn1.addEventListener('mouseleave', changeText)
-btn2.addEventListener('mouseleave', changeText)
-
-function changeText () {
-        const tmp = btn1.innerText;
-        btn1.innerText = btn2.innerText;
-        btn2.innerText = tmp
+for (const btn of btnCollection) {
+    btn.addEventListener('click', clickHandler)
 }
 
+function clickHandler ({target: {parentNode, dataset: {color}}} ) {
+   parentNode.style.backgroundColor = color;
+}
