4d45face544300ee261f1f3d9f711dd960eed54b
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-30 12:26:33 +0200

    Dynamic creating

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
+/*
+Динамічне створення елементу
+1. Створити об'єкт елемента і отримати адресу цього об'єкта в пам'яті
+2. Доєднати елемент до батьківського (приаппендити)
+*/
 
-const form = document.querySelector('form');
-const resultField = document.querySelector('#resultField');
-const KOEFFICIENT = {
-    USD: 36.6,
-    EUR: 35.76,
-    PLN: 7.47,
-    UAH: 1
-}
-
-form.addEventListener('submit', submitHandler);
-
-function submitHandler (event) {
-    event.preventDefault();
-    const {target: {amount: {value: amount}, 
-            currencyInput: {value: currencyInput}, 
-            currencyOutput: {value: currencyOutput}}} = event;
-    if(Number.isNaN(Number(amount))) {
-        resultField.textContent = 'Amount must be a number';
-    } else {
-        const res = amount * KOEFFICIENT[currencyInput] / KOEFFICIENT[currencyOutput]
-        resultField.textContent = res.toFixed(2);
-    }
-}
+const element = document.createElement('h1');
+element.textContent = 'Hello, DOM';
+document.body.append(element);
