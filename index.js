61773a8abde2386e781032d33663b1c53b113a44
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-11-02 16:27:20 +0200

    refactor stage 1

--- index.js
+++ index.js
@@ -1,36 +1,49 @@
-/* TODO:
-Частина 1:
-	@@ -16,4 +13,34 @@ https://pbs.twimg.com/media/EIopEQ6XYAEqzUk.jpg
-Якщо обрати іншу картку, то з попередньої картки підсвітка знімається.
-Тобто одночасно може бути обрана тільки одна картка.
-*/
-
-const root = document.querySelector('#root');
-
 const cardArray = userData.map(user => createUserCard(user));
 
+root.append(...cardArray);
+
+/* TODO:
+1. Refactor repeating code
+	@@ -28,31 +29,33 @@ const cardArray = userData.map(user => createUserCard(user));
+*/
+
 function createUserCard(user){
-
-    const wrapper = document.createElement('div');
-    wrapper.classList.add('card-wrapper');
-
-    const img = document.createElement('img');
+      const img = document.createElement('img');
     img.setAttribute('src', user.profilePicture);
     img.setAttribute('alt', user.name);
     img.classList.add('avatar');
 
-    const h2 = document.createElement('h2');
-    h2.append(user.name);
-
-    const p = document.createElement('p');
-    p.classList.add('desc');
-    p.append(user.description);
-
-    const button = document.createElement('button');
-    button.append('Connent');
-
-    wrapper.append(img, h2, p, button);
+    const h2 = createElement('h2', {}, user.name);
+    const p = createElement('p', {classNames: ['desc']}, user.description);
+    const button = createElement('button', {}, 'Connect');
+    const wrapper = createElement('div', {classNames: ['card-wrapper']}, img, h2, p, button);
+    wrapper.addEventListener('click', getActiveCard);
     return wrapper;
 }
 
-root.append(...cardArray);
+/**
+ * 
+ * @param {String} type 
+ * @param {Object} options 
+ * @param {String[]} options.classNames
+ * @param  {Node} children 
+ */
+
+function createElement(type, {classNames = []}, ...children) {
+    const elem = document.createElement(type);
+    elem.classList.add(...classNames);
+    elem.append(...children);
+    return elem;
+}
+
+
+function getActiveCard(event){
+    const activeCard = document.querySelector('.active');
+    if (activeCard === event.currentTarget) {
+        return;
+    }
+    if(activeCard){
+        activeCard.classList.remove('active');
+    }
+    event.currentTarget.classList.add('active');
+}
