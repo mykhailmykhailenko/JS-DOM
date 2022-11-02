7454b8df9f6314055b21403273d5be9527357761
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-11-02 16:21:23 +0200

    vacation task start resolving

--- index.js
+++ index.js
@@ -1,65 +1,36 @@
 /* TODO:
 Частина 1:
-Динамічно створити картки юзерів з даними, що містяться в масиві userData
-Приклад вигляду картки юзера:
-https://pbs.twimg.com/media/EIopEQ6XYAEqzUk.jpg
-(всі картки одночасно на екрані)
-В html тільки <div id="root"></div>
-Частина 2:
-За натиснення на картку юзера картка має підсвітитися червоним бордером - картка "обрана"
+	@@ -16,4 +13,34 @@ https://pbs.twimg.com/media/EIopEQ6XYAEqzUk.jpg
 Якщо обрати іншу картку, то з попередньої картки підсвітка знімається.
 Тобто одночасно може бути обрана тільки одна картка.
 */
 
-// console.log(userData);
-
 const root = document.querySelector('#root');
 
-function logger(event) {
-    const currentUserCard = event.currentTarget;
-    currentUserCard.style.border = '3px solid red';
-    currentUserCard.removeEventListener('click', logger);
- }
+const cardArray = userData.map(user => createUserCard(user));
 
-function createUser(obj) {
+function createUserCard(user){
 
-    const div = document.createElement('div');
-    div.classList.add('userCard');
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('card-wrapper');
 
     const img = document.createElement('img');
-    img.setAttribute('src', obj.profilePicture);
-    img.classList.add('userPhoto');
+    img.setAttribute('src', user.profilePicture);
+    img.setAttribute('alt', user.name);
+    img.classList.add('avatar');
 
-    const h1 = document.createElement('h1');
-    h1.innerText = `${obj.name}`;
-    h1.classList.add('userName');
+    const h2 = document.createElement('h2');
+    h2.append(user.name);
 
     const p = document.createElement('p');
-    p.innerText = `${obj.description}`;
-    p.classList.add('userInfo');
+    p.classList.add('desc');
+    p.append(user.description);
 
     const button = document.createElement('button');
-    button.innerText = "Connect";
-    button.classList.add('btn');
+    button.append('Connent');
 
-    div.append(img, h1, p, button);
-
-    return div
+    wrapper.append(img, h2, p, button);
+    return wrapper;
 }
 
-
-const userArray = userData.map(createUser);
-
-
-for (let user of userArray) {
-    user.addEventListener ('click', logger);
-}
-
-function logger (event) {
-    for (let user of userArray) {
-        user.style.border = '1px solid black'
-    }
-    event.currentTarget.style.border = '5px solid red'  
-}
-
-root.append(...userArray);
+root.append(...cardArray);
